e5faad0e21e6a03b7caaac2e0b7f43f36dbb0b5f
arun adiraju <arparwan@gmail.com>
2017-11-27 00:26:49 +0530

    revert bad json parsing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,13 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { CameraPreview } from '@ionic-native/camera-preview';
+import { HTTP } from '@ionic-native/http';
+import { Base64ToGallery } from '@ionic-native/base64-to-gallery';
+
+import { File } from '@ionic-native/file';
+import { Transfer } from '@ionic-native/transfer';
+import { FilePath } from '@ionic-native/file-path';
+import { Camera } from '@ionic-native/camera';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -26,6 +33,12 @@ import { HomePage } from '../pages/home/home';
     StatusBar,
     SplashScreen,
     CameraPreview,
+    HTTP,
+    Base64ToGallery,
+    File,
+    Transfer,
+    Camera,
+    FilePath,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { CameraPreview, CameraPreviewPictureOptions } from '@ionic-native/camera-preview';
+import { Base64ToGallery } from '@ionic-native/base64-to-gallery';
 import { LoadingController } from 'ionic-angular';
+import { HTTP } from '@ionic-native/http';
+import { errorHandler } from '@angular/platform-browser/src/browser';
 
+import { ActionSheetController, ToastController, Platform, Loading } from 'ionic-angular';
 
+import { File } from '@ionic-native/file';
+import { Transfer, TransferObject } from '@ionic-native/transfer';
+import { FilePath } from '@ionic-native/file-path';
+import { Camera } from '@ionic-native/camera';
+
+declare var cordova: any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -11,8 +21,19 @@ import { LoadingController } from 'ionic-angular';
 export class HomePage {
   picture;
   info;
+  imageLocation;
 
-  constructor(public navCtrl: NavController, public cameraPreview: CameraPreview, public loadingCtrl: LoadingController) {
+  lastImage: string = null;
+  loading: Loading;
+
+  constructor(public navCtrl: NavController, public cameraPreview: CameraPreview,
+    public loadingCtrl: LoadingController,
+    public http: HTTP,
+    public base64ToGallery: Base64ToGallery,
+    public platform: Platform,
+    public toastCtrl: ToastController,
+    private camera: Camera, private transfer: Transfer, private file: File, private filePath: FilePath
+  ) {
     this.info = 'Capture Image';
   }
 
@@ -26,14 +47,78 @@ export class HomePage {
 
     // take a picture
     this.cameraPreview.takePicture(pictureOpts).then((imageData) => {
+      
       this.presentLoading();
       this.picture = 'data:image/jpeg;base64,' + imageData;
       this.info = 'Image Captured';
       console.log('Capture image success');
+
+      /*var fileNamePrefix = this.createFileName();
+      this.base64ToGallery.base64ToGallery(imageData).then(
+        res => {this.info = 'saved image' + res},
+        err => {this.info = 'failed to save image' + err}
+      );
+      */
+    
+      
+      var headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+      };
+
+      var postData = {
+        "image" : imageData 
+      };
+      //this.http.setHeader('mimetype', 'multipart/form-data');
+      this.http.post('http://35.193.71.148/mobile/upload', postData, headers)
+      .then(data => {
+
+        console.log(data.status);
+        console.log(data.data); // data received by server
+        console.log(data.headers);
+        this.info = data.data;
+        this.imageLocation = this.picture; 
+        /*var json = JSON.parse(data.data);
+        var uploadedFileName = json.file_name;
+
+        var similarUrl = 'http://35.193.71.148/mobile/similar?file_name=' + uploadedFileName;
+
+        this.http.get(similarUrl, {}, {})
+        .then(data => {
+      
+          var respsonseSimilar = JSON.parse(data.data);
+          var similarUrl = respsonseSimilar.url[1];
+          this.imageLocation = similarUrl;
+        })
+        .catch(error => {
+      
+          console.log(error.status);
+          console.log(error.error); // error message as string
+          console.log(error.headers);
+      
+        });
+        */
+
+      })
+      .catch(error => {
+
+        console.log(error.status);
+        console.log(error.error); // error message as string
+        console.log(error.headers);
+        this.info = 'errror : ' + error + 'error status : ' + error.status + ' error message : ' + error.error;
+
+      });
+
     }, (err) => {
       console.log('Capture image failed');
       console.log(err);
     });
+  }
+
+  saveImage(imageData) {
+    this.base64ToGallery.base64ToGallery(imageData, { prefix: '_img' }).then(
+      res => console.log('Saved image to gallery ', res),
+      err => console.log('Error saving image to gallery ', err)
+    );
   }
 
   presentLoading() {
@@ -44,4 +129,127 @@ export class HomePage {
     loader.present();
   }
 
+  getRequest() {
+    this.http.get('http://35.193.71.148/static/inputFiles/menShirt/n04197391_10607_0.jpg', {}, {})
+    .then(data => {
+  /*
+      console.log(data.status);
+      console.log(data.data); // data received by server
+      console.log(data.headers);
+      */
+      this.imageLocation = "http://35.193.71.148/static/inputFiles/menShirt/n04197391_10607_0.jpg";
+    })
+    .catch(error => {
+  
+      console.log(error.status);
+      console.log(error.error); // error message as string
+      console.log(error.headers);
+  
+    });
+  }
+
+  public takePicture() {
+    // Create options for the Camera Dialog
+    var options = {
+      quality: 100,
+      //sourceType: sourceType,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    };
+   
+    // Get the data of an image
+    this.camera.getPicture(options).then((imagePath) => {
+      // Special handling for Android library
+     /*}if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+        this.filePath.resolveNativePath(imagePath)
+          .then(filePath => {
+            let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+            let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+          });
+       else {
+        var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+        var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+        this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+      }*/
+      var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+      var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+      this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+      this.uploadImage();
+    }, (err) => {
+      this.presentToast('Error while selecting image.');
+    });
+  }
+
+  // Create a new name for the image
+private createFileName() {
+  var d = new Date(),
+  n = d.getTime(),
+  newFileName =  n + ".jpg";
+  return newFileName;
+}
+ 
+// Copy the image to a local folder
+private copyFileToLocalDir(namePath, currentName, newFileName) {
+  this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+    this.lastImage = newFileName;
+  }, error => {
+    this.presentToast('Error while storing file.');
+  });
+}
+ 
+private presentToast(text) {
+  let toast = this.toastCtrl.create({
+    message: text,
+    duration: 3000,
+    position: 'bottom'
+  });
+  toast.present();
+}
+ 
+// Always get the accurate path to your apps folder
+public pathForImage(img) {
+  if (img === null) {
+    return '';
+  } else {
+    return cordova.file.dataDirectory + img;
+  }
+}
+
+  public uploadImage() {
+    // Destination URL
+    var url = 'http://35.193.71.148/mobile/upload';
+   
+    // File for Upload
+    var targetPath = this.pathForImage(this.lastImage);
+   
+    // File name only
+    var filename = this.lastImage;
+   
+    var options = {
+      fileKey: "file",
+      fileName: filename,
+      chunkedMode: false,
+      mimeType: "multipart/form-data",
+      params : {'image': filename}
+    };
+   
+    const fileTransfer: TransferObject = this.transfer.create();
+   
+    this.loading = this.loadingCtrl.create({
+      content: 'Uploading...',
+    });
+    this.loading.present();
+   
+    // Use the FileTransfer to upload the image
+    fileTransfer.upload(targetPath, url, options).then(data => {
+      this.loading.dismissAll()
+      this.presentToast('Image succesful uploaded.');
+    }, err => {
+      this.loading.dismissAll()
+      this.presentToast('Error while uploading file.');
+    });
+  }
+
+  
 }
